ca3c830a12e4b6cc16749a227975784f9f4b8916
FORTIN Victor <victor.fortin078@gmail.com>
2025-09-26 22:48:48 +0200

    finis action php

--- action.php
+++ action.php
@@ -5,23 +5,26 @@
 
 
 if (isset( $_POST['OK'], $_POST['nom'], $_POST['prenom'], $_POST['age'])) {
-    if (is_string($_POST['nom']) and strlen($_POST['nom']) >= 3 and strlen($_POST['nom']) <= 15){
+
+
+    if (is_string($_POST['nom']) and is_string($_POST['prenom']) and is_numeric($_POST['age']) and $_POST['age'] >= 0) {
+
+
+
         $nom = $_POST['nom'];
-    }
-    if (is_string($_POST['prenom']) and strlen($_POST['prenom']) >= 3 and strlen($_POST['prenom']) <= 15){
         $prenom = $_POST['prenom'];
-    }
-
-    if (is_int($_POST['age']) and $_POST['age'] >= 0) {
         $age = $_POST['age'];
+
+        $result = array($nom, $prenom, $age);
+
+        fputcsv($fp, $result);
+        fclose($fp);
+        $_POST['nom'] = null;
+        $_POST['prenom'] = null;
+        $_POST['age'] = null;
+
     }
-
-    $age = $_POST['age'];
-    $result = array($nom, $prenom, $age);
-
-    fputcsv($fp, $result);
-    fclose($fp);
 
 }
+    include ('lecture.php');
 
-?>
